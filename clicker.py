a3be521a5acf1996276ba7cc2dc02823249a7954
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-07 00:41:26 +0200

    Add pygame base

--- clicker.py
+++ clicker.py
@@ -1,5 +1,25 @@
+import pygame
+
+class Business:
+    def __init__(self):
+        self.credits = 0
+
+def update_screen(b):
+    print(f'[MINE]\n Credits: {b.credits}')      
+
 def main():
-    print('[CLICKER]')
+    WIDTH, HEIGHT = 960, 540
+    
+    pygame.init()
+    pygame.display.set_mode((WIDTH, HEIGHT))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                run = False
+            
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
