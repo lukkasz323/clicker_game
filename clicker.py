59a5a02b2d7d9f80b6a5ef1a705992eb54b0363d
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-07 20:48:21 +0200

    Add button support, other minor changes

--- clicker.py
+++ clicker.py
@@ -3,6 +3,10 @@ import pygame
 class Business:
     def __init__(self):
         self.credits = 0
+        self.stone = 0
+
+def text(string, font, color='white'):
+    return font.render(string, True, color)
 
 def main():
     WIDTH, HEIGHT = 512, 512
@@ -14,10 +18,9 @@ def main():
     W = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Mine Clicker')
     
-    FONT_SMALL = pygame.font.SysFont('tahoma', 18)
-    FONT_MEDIUM = pygame.font.SysFont('tahoma', 20)
-    FONT_LARGE = pygame.font.SysFont('tahoma', 32)
-    text_logo = FONT_LARGE.render('Mine Clicker', True, 'yellow')
+    FONT_S = pygame.font.SysFont('tahoma', 18)
+    FONT_M = pygame.font.SysFont('tahoma', 20)
+    FONT_L = pygame.font.SysFont('tahoma', 32)
     
     clock = pygame.time.Clock()
     b = Business()
@@ -29,20 +32,33 @@ def main():
         clock.tick(FRAMERATE)
         
         # Handle events.
+        event_mousebuttondown = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                event_mousebuttondown = True
                 
         # Logic
         fps = round(clock.get_fps())
-        text_fps = FONT_SMALL.render(f'{fps}', True, 'white')
-        text_credits = FONT_MEDIUM.render(f'Credits: {b.credits}', True, 'white')
+        mouse_pos = pygame.mouse.get_pos()
+        
+        text_fps = text(f'{fps}', FONT_M)
+        text_credits = text(f'Credits: {b.credits}', FONT_M)
+        text_stone = text(f'Stone: {b.stone}', FONT_M)
+        
+        BUTTON_SELL_STONE = pygame.Rect(8, 64, 32, 32)
+        BUTTON_TEST = pygame.Rect(18, 164, 32, 32)
+        
+        if event_mousebuttondown and pygame.mouse.get_pressed()[0] and BUTTON_SELL_STONE.collidepoint(mouse_pos):
+            print(1)
         
         # Draw
         W.fill((16, 16, 16)) # Background
-        W.blit(text_logo, (16, 8))
-        W.blit(text_fps, (480, 8))
-        W.blit(text_credits, (12, 64))
+        W.fill('gray20', BUTTON_SELL_STONE)
+        W.blits(((text_fps, (480, 8)),
+                (text_credits, (16, 16)),
+                (text_stone, (48, 64))))
         
         # Update screen.
         pygame.display.update()
