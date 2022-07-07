4552d413510a6115f63d8ab0b766cd1ff7947683
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-07 08:41:53 +0200

    Add fonts, logo, fps counter, Remove click test,
    Change window width, height

--- clicker.py
+++ clicker.py
@@ -5,13 +5,20 @@ class Business:
         self.credits = 0
 
 def main():
-    WIDTH, HEIGHT = 960, 540
+    WIDTH, HEIGHT = 512, 512
     FRAMERATE = 60
     
     # Initial tasks.
     pygame.init()
+    
     W = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Mine Clicker')
+    
+    FONT_SMALL = pygame.font.SysFont('tahoma', 16)
+    FONT_MEDIUM = pygame.font.SysFont('tahoma', 24)
+    FONT_LARGE = pygame.font.SysFont('tahoma', 32)
+    logo = FONT_LARGE.render('Mine Clicker', True, 'yellow')
+    
     clock = pygame.time.Clock()
     
     # Tasks ran every frame.
@@ -24,11 +31,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] is True:
-                print('LMB down!')
+                
+        # Logic
+        fps = round(clock.get_fps())
+        text_fps = FONT_SMALL.render(f'{fps}', True, 'white')
         
         # Draw
         W.fill((16, 16, 16)) # Background
+        W.blit(logo, (16, 8))
+        W.blit(text_fps, (480, 8))
         
         # Update screen.
         pygame.display.update()
