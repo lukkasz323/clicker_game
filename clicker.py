4040f27d056d9eefd441878e7500e591d00fe503
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-07 05:00:21 +0200

    Expand basic functionality

--- clicker.py
+++ clicker.py
@@ -9,16 +9,27 @@ def update_screen(b):
 
 def main():
     WIDTH, HEIGHT = 960, 540
+    FRAMERATE = 60
     
     pygame.init()
-    pygame.display.set_mode((WIDTH, HEIGHT))
-
+    W = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Mine Clicker')
+    clock = pygame.time.Clock()
+    
     run = True
     while run:
+        # Keep steady framerate.
+        clock.tick(FRAMERATE)
+        
+        # Handle 'quit' request.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
+            if event.type == pygame.QUIT:
                 run = False
-            
+        
+        # Update screen.
+        W.fill((16, 16, 16))
+        pygame.display.update()
+        
     pygame.quit()
 
 if __name__ == '__main__':
