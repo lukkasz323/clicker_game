e7d99c2017125ff6b326ade6b4336544772a439b
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-08 02:47:14 +0200

    Add clicker and resource selling

--- clicker.py
+++ clicker.py
@@ -3,7 +3,11 @@ import pygame
 class Business:
     def __init__(self):
         self.credits = 0
-        self.stone = 0
+        self.stone = {'amount': 0, 'value': 1}
+        
+    def sell(self, material):
+        self.credits += material['amount'] * material['value']
+        material['amount'] = 0
 
 def text(string, font, color='white'):
     return font.render(string, True, color)
@@ -45,17 +49,21 @@ def main():
         
         text_fps = text(f'{fps}', FONT_M)
         text_credits = text(f'Credits: {b.credits}', FONT_M)
-        text_stone = text(f'Stone: {b.stone}', FONT_M)
+        text_stone = text(f"Stone: {b.stone['amount']}", FONT_M)
         
+        BUTTON_MINE = pygame.Rect(240, 16, 32, 32)
         BUTTON_SELL_STONE = pygame.Rect(8, 64, 32, 32)
-        BUTTON_TEST = pygame.Rect(18, 164, 32, 32)
         
-        if event_mousebuttondown and pygame.mouse.get_pressed()[0] and BUTTON_SELL_STONE.collidepoint(mouse_pos):
-            print(1)
+        if event_mousebuttondown and pygame.mouse.get_pressed()[0]:
+            if BUTTON_MINE.collidepoint(mouse_pos):
+                b.stone['amount'] += 1
+            if BUTTON_SELL_STONE.collidepoint(mouse_pos):
+                b.sell(b.stone)
         
         # Draw
         W.fill((16, 16, 16)) # Background
-        W.fill('gray20', BUTTON_SELL_STONE)
+        W.fill('yellow', BUTTON_MINE)
+        W.fill('gray30', BUTTON_SELL_STONE)
         W.blits(((text_fps, (480, 8)),
                 (text_credits, (16, 16)),
                 (text_stone, (48, 64))))
