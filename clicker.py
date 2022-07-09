87f309b19efe52a0e4cd3cbb60fce3aa7d4ad738
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-09 18:57:59 +0200

    Add new material, change material handling

--- clicker.py
+++ clicker.py
@@ -3,7 +3,8 @@ import pygame
 class Business:
     def __init__(self):
         self.credits = 0
-        self.stone = {'value': 1, 'amount': 0, 'miners': 0, 'miner_cost': 10}
+        self.stone = {'value': 1, 'miner_cost': 10, 'miner_cost_multiplier': 2, 'amount': 0, 'miners': 0}
+        self.copper = {'value': 5, 'miner_cost': 64, 'miner_cost_multiplier': 4, 'amount': 0, 'miners': 0}
         
     def sell(self, material):
         self.credits += material['amount'] * material['value']
@@ -12,6 +13,7 @@ class Business:
     def buy_miner(self, material):
         if self.credits >= material['miner_cost']:
             self.credits -= material['miner_cost']
+            material['miner_cost'] *= material['miner_cost_multiplier']
             material['miners'] += 1
 
 def text(string, font, color='white'):
@@ -32,6 +34,12 @@ def main():
     FONT_S = pygame.font.SysFont('tahoma', 18)
     FONT_M = pygame.font.SysFont('tahoma', 20)
     FONT_L = pygame.font.SysFont('tahoma', 32)
+
+    BUTTON_MINE = pygame.Rect(240, 16, 32, 32)
+    BUTTON_BUY_MINER_STONE = pygame.Rect(240, 64, 32, 32)
+    BUTTON_BUY_MINER_COPPER = pygame.Rect(240, 112, 32, 32)
+    BUTTON_SELL_STONE = pygame.Rect(8, 64, 32, 32)
+    BUTTON_SELL_COPPER = pygame.Rect(8, 112, 32, 32)
     
     TIMER_1000 = pygame.event.custom_type()
     pygame.time.set_timer(TIMER_1000, 1000)
@@ -57,35 +65,42 @@ def main():
         fps = round(clock.get_fps())
         mouse_pos = pygame.mouse.get_pos()
         
-        BUTTON_MINE = pygame.Rect(240, 16, 32, 32)
-        BUTTON_BUY_MINER_STONE = pygame.Rect(240, 64, 32, 32)
-        BUTTON_SELL_STONE = pygame.Rect(8, 64, 32, 32)
-        
         if event_timer_1000:
             b.stone['amount'] += b.stone['miners']
+            b.copper['amount'] += b.copper['miners']
         
         if event_mousebuttondown and pygame.mouse.get_pressed()[0]:
             if BUTTON_MINE.collidepoint(mouse_pos):
                 b.stone['amount'] += 1
             if BUTTON_BUY_MINER_STONE.collidepoint(mouse_pos):
                 b.buy_miner(b.stone)
+            if BUTTON_BUY_MINER_COPPER.collidepoint(mouse_pos):
+                b.buy_miner(b.copper)
             if BUTTON_SELL_STONE.collidepoint(mouse_pos):
                 b.sell(b.stone)
+            if BUTTON_SELL_COPPER.collidepoint(mouse_pos):
+                b.sell(b.copper)
         
         # Draw
         text_fps = text(f'{fps}', FONT_M)
         text_credits = text(f'Credits: {b.credits}', FONT_M)
         text_stone = text(f"Stone: {b.stone['amount']} [+{b.stone['miners']}/s]", FONT_M)
+        text_copper = text(f"Copper: {b.copper['amount']} [+{b.copper['miners']}/s]", FONT_M)
         text_miner_stone = text(f"{b.stone['miner_cost']}", FONT_M)
+        text_miner_copper = text(f"{b.copper['miner_cost']}", FONT_M)
         
         W.fill((16, 16, 16)) # Background
         W.fill('yellow', BUTTON_MINE)
         W.fill('gray', BUTTON_BUY_MINER_STONE)
+        W.fill('gray', BUTTON_BUY_MINER_COPPER)
         W.fill('gray30', BUTTON_SELL_STONE)
+        W.fill('gray30', BUTTON_SELL_COPPER)
         W.blits(((text_fps, (480, 8)),
                 (text_credits, (16, 16)),
                 (text_stone, (48, 64)),
-                (text_miner_stone, (288, 64))))
+                (text_copper, (48, 112)),
+                (text_miner_stone, (288, 64)),
+                (text_miner_copper, (288, 112))))
         
         # Update screen.
         pygame.display.update()
