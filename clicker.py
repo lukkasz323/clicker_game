36b651c2ae1dfefe603c4c3ac380b4677bbd3009
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-09 06:10:15 +0200

    Add miners (non-functional yet)

--- clicker.py
+++ clicker.py
@@ -3,11 +3,11 @@ import pygame
 class Business:
     def __init__(self):
         self.credits = 0
-        self.stone = {'amount': 0, 'value': 1}
+        self.stone = {'value': 1, 'amount': 0, 'miners': 0}
         
     def sell(self, material):
-        self.credits += material['amount'] * material['value']
         material['amount'] = 0
+        self.credits += material['amount'] * material['value']
 
 def text(string, font, color='white'):
     return font.render(string, True, color)
@@ -49,20 +49,24 @@ def main():
         
         text_fps = text(f'{fps}', FONT_M)
         text_credits = text(f'Credits: {b.credits}', FONT_M)
-        text_stone = text(f"Stone: {b.stone['amount']}", FONT_M)
+        text_stone = text(f"Stone: {b.stone['amount']} [+{b.stone['miners']}/s]", FONT_M)
         
         BUTTON_MINE = pygame.Rect(240, 16, 32, 32)
+        BUTTON_BUY_MINER_STONE = pygame.Rect(240, 64, 32, 32)
         BUTTON_SELL_STONE = pygame.Rect(8, 64, 32, 32)
         
         if event_mousebuttondown and pygame.mouse.get_pressed()[0]:
             if BUTTON_MINE.collidepoint(mouse_pos):
                 b.stone['amount'] += 1
+            if BUTTON_BUY_MINER_STONE.collidepoint(mouse_pos):
+                b.stone['miners'] += 1
             if BUTTON_SELL_STONE.collidepoint(mouse_pos):
                 b.sell(b.stone)
         
         # Draw
         W.fill((16, 16, 16)) # Background
         W.fill('yellow', BUTTON_MINE)
+        W.fill('gray', BUTTON_BUY_MINER_STONE)
         W.fill('gray30', BUTTON_SELL_STONE)
         W.blits(((text_fps, (480, 8)),
                 (text_credits, (16, 16)),
